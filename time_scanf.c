f390f701105602efab6d785b3fec050404b80e71
Prince Singh <83390756+singhprinz@users.noreply.github.com>
2021-05-22 14:59:52 +0530

    Delete time_scanf.c

--- time_scanf.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int hr,min,sec;
-    printf("enter the time (in HH:MM:SS)");
-    scanf("%02d:%02d:%02d",&hr,&min,&sec);
-    printf("Entered time is %02d:%02d:%02d\n",hr,min,sec);
-    return 0;
-}
